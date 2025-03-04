0ace2c8c2bd33f411f02055db2a82f09dc20a29a
Victoria <vv74t@mail.ru>
2025-03-04 12:47:18 +0500

    Deployment to GitHub Pages is configured

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-module.exports = {
+export default {
     root: './',
+    base: '/5S_Work_Flow/', // Укажите имя репозитория
     build: {
         outDir: 'dist',
     },
     publicDir: 'assets'
- } 
+}
