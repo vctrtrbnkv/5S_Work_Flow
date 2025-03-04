8623fc5f41fdd5a0415e20d28032023f46131d29
Victoria <vv74t@mail.ru>
2025-03-04 13:06:41 +0500

    corrected file paths

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -16,28 +16,28 @@ export default class PreloadScene extends CustomScene {
 
         this.load.multiatlas('figures', './assets/sprites/figures.json', 'sprites');
 
-        this.load.audio('drag-01', 'assets/sounds/drag-01.mp3');
+        this.load.audio('drag-01', './assets/sounds/drag-01.mp3');
 
-        this.load.audio('drop-01', 'assets/sounds/drop-01.mp3');
-        this.load.audio('drop-02', 'assets/sounds/drop-02.mp3');
-        this.load.audio('drop-03', 'assets/sounds/drop-03.mp3');
+        this.load.audio('drop-01', './assets/sounds/drop-01.mp3');
+        this.load.audio('drop-02', './assets/sounds/drop-02.mp3');
+        this.load.audio('drop-03', './assets/sounds/drop-03.mp3');
 
-        this.load.audio('paper-01', 'assets/sounds/paper-01.mp3');
-        this.load.audio('paper-02', 'assets/sounds/paper-02.mp3');
-        this.load.audio('paper-03', 'assets/sounds/paper-03.mp3');
+        this.load.audio('paper-01', './assets/sounds/paper-01.mp3');
+        this.load.audio('paper-02', './assets/sounds/paper-02.mp3');
+        this.load.audio('paper-03', './assets/sounds/paper-03.mp3');
 
-        this.load.audio('pencil-01', 'assets/sounds/pencil-01.mp3');
-        this.load.audio('pencil-02', 'assets/sounds/pencil-02.mp3');
-        this.load.audio('pencil-03', 'assets/sounds/pencil-03.mp3');
+        this.load.audio('pencil-01', './assets/sounds/pencil-01.mp3');
+        this.load.audio('pencil-02', './assets/sounds/pencil-02.mp3');
+        this.load.audio('pencil-03', './assets/sounds/pencil-03.mp3');
 
-        this.load.audio('camera-shutter', 'assets/sounds/camera-shutter.mp3');
-        this.load.audio('swoosh-modal', 'assets/sounds/swoosh-modal.mp3');
+        this.load.audio('camera-shutter', './assets/sounds/camera-shutter.mp3');
+        this.load.audio('swoosh-modal', './assets/sounds/swoosh-modal.mp3');
 
-        this.load.audio('background-music', 'assets/sounds/background-music.mp3');
+        this.load.audio('background-music', './assets/sounds/background-music.mp3');
 
-        this.load.audio('success', 'assets/sounds/success.mp3');
+        this.load.audio('success', './assets/sounds/success.mp3');
 
-        this.load.audio('mouse-click-sound', 'assets/sounds/mouse-click-sound.mp3');
+        this.load.audio('mouse-click-sound', './assets/sounds/mouse-click-sound.mp3');
     }
 
     create() {
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 export default {
     root: './',
-    base: '/5S_Work_Flow', // Укажите имя репозитория
+    base: '/5S_Work_Flow',
     build: {
         outDir: 'dist',
     },
